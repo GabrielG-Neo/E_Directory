ff1c307572a50a670061ee57e2be5ff302ccc705
NeoGabe <gabrielg28@hotmail.com>
2021-03-14 10:38:16 -0500

    components

---
+++ employeeinfo/src/components/EmployeeCard/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+function EmployeeCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>First Name:</strong> {props.firstName}
+          </li>
+          <li>
+            <strong>Last Name:</strong> {props.lastName}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.email}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default FriendCard;
