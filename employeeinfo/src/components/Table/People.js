ce81710f172399361af541c3e9c9068a159c6b32
NeoGabe <gabrielg28@hotmail.com>
2021-03-13 21:47:09 -0600

    inital commit redo

---
+++ employeeinfo/src/components/Table/People.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import API from "../../utils/API";
+
+export default function people() {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                </tr> 
+               
+            </table>
+        </div>
+    )
+}
+
+
