a724f63797ec60aa14b470a570c490a912cf6779
NeoGabe <gabrielg28@hotmail.com>
2021-03-14 10:43:28 -0500

    modification to components

--- employeeinfo/src/Search.js
+++ employeeinfo/src/Search.js
@@ -3,14 +3,12 @@ import API from "../utils/API";
 import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+
 
 class Search extends Component {
   state = {
     search: "",
-    breeds: [],
-    results: [],
-    error: ""
+    name: []
   };
 
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
--- employeeinfo/src/components/EmployeeCard/index.js
+++ employeeinfo/src/components/EmployeeCard/index.js
@@ -24,4 +24,4 @@ function EmployeeCard(props) {
   );
 }
 
-export default FriendCard;
+export default EmployeeCard;
